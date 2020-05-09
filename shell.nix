cdd6fad6a86a6e8287e45641f23b4d67aef3204e
ナッティ <hello@nutty.cc>
2020-05-08 22:20:53 -0700

    Install PureScript tooling

--- shell.nix
+++ shell.nix
@@ -6,12 +6,20 @@ let
     exes = [ "neocities" ];
     gemdir = ./bin/neocities;
   };
+
+  easyPS = import (pkgs.fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "d4879bfd2b595d7fbd37da1a7bea5d0361975eb3";
+    sha256 = "0kzwg3mwziwx378kvbzhayy65abvk1axi12zvf2f92cs53iridwh";
+  }) {};
 in
   pkgs.mkShell {
     name = "nutty.world";
-    buildInputs = [
+    buildInputs = easyPS.buildInputs ++ [
       pkgs.bundix
       pkgs.gnumake
+      pkgs.yarn
       neocities
     ];
   }
